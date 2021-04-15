2e4ce86714c3e06644cf47400dc5357d3e7ba93e
Kate Reeher <kreeher@korterra.com>
2021-04-15 16:49:58 -0500

    pipenv

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,16 +11,19 @@ DEFAULT_DATE = 'fs'
 
 PATH = 'content'
 
+PLUGINS= None
 # tipue doesn't work with pip install for plugins ;_;
-PLUGIN_PATHS = ["plugins"]
-PLUGINS = ['tipue_search']
+#PLUGIN_PATHS = ["plugins"]
+#PLUGINS = ['tipue_search']
 
 # these are required for tipue
-DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'search', '404']
+#DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'search', '404']
 
 TIMEZONE = 'America/Chicago'
 
-THEME = 'themes/elegant' #'../pelican-themes/basic'
+THEME_STATIC_DIR = 'theme'
+#THEME = 'themes/elegant' #
+THEME= 'pelican-themes/storm'
 
 DEFAULT_LANG = u'en'
 
